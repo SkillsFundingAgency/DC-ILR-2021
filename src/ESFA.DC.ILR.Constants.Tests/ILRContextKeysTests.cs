ed553adf4153422fd03302abb1b49201241193c2
Mike Childs <mike@mjcdev.co.uk>
2019-05-08 13:21:13 +0100

    Created ILR Constants Project, Tests, and Nuget Package, as placeholder for fully fleshed out set of Context Keys for ILR 1920

---
+++ src/ESFA.DC.ILR.Constants.Tests/ILRContextKeysTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ESFA.DC.ILR.Constants.Tests
+{
+    public class ILRContextKeysTests
+    {
+        [InlineData(ILRContextKeys.Container, "Container")]
+        [InlineData(ILRContextKeys.Filename, "Filename")]
+        [InlineData(ILRContextKeys.OriginalFilename, "OriginalFilename")]
+        [InlineData(ILRContextKeys.ValidationErrors, "ValidationErrors")]
+        [Theory]
+        public void ILRContextKeyValue(string actual, string expected)
+        {
+            actual.Should().Be(expected);
+        }
+    }
+}
